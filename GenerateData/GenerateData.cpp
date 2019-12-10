0bf796de91078e7f8f766128017837df67e9da72
Martin Piper <martin.piper@gmail.com>
2019-12-11 00:05:11 +0800

    Investigating multiplexed sprites and hardware limits

--- GenerateData/GenerateData.cpp
+++ GenerateData/GenerateData.cpp
@@ -15,10 +15,10 @@ int main(int argc, char**argv)
 	const int maxFrames = 512;
 
 	double rads1 = 0, rads2 = 0, rads3 = 0, rads4 = 0, rads5 = 0;
-	const double rads1Speed = M_PI / 100 , rads2Speed = M_PI / 173, rads3Speed = M_PI / 40, rads4Speed = M_PI / 200, rads5Speed = M_PI / 240;
-	const double rads1Separation = M_PI / 20, rads2Separation = M_PI / 30, rads3Separation = M_PI / 10;
-	const double radius1x = 70, radius2x = 30, radius3x = 30;
-	const double radius1y = 70, radius2y = 20;
+	const double rads1Speed = M_PI / 100 , rads2Speed = M_PI / 173, rads3Speed = M_PI / 123, rads4Speed = M_PI / 200, rads5Speed = M_PI / 240;
+	const double rads1Separation = M_PI / 20, rads2Separation = M_PI / 30, rads3Separation = M_PI / 15;
+	const double radius1x = 70, radius2x = 30, radius3x = 100;
+	const double radius1y = 50, radius2y = 20;
 
 	const char *paletteHiNybbles = "00000000244428882ccc2000";
 
@@ -42,7 +42,7 @@ int main(int argc, char**argv)
 		{
 			file << "b$" << std::hex << std::setw(2) << 0x10+i;
 			file << ",b$" << paletteHiNybbles[i] << std::hex << std::setw(1) << ((i / 8)&0xf);
-			file << ",b$" << std::hex << std::setw(2) << (int)(128 + (cos(rads1Real) * radius1y) + (sin(rads2Real) * radius2y));
+			file << ",b$" << std::hex << std::setw(2) << (int)(150 + (cos(rads1Real) * radius1y) + (sin(rads2Real) * radius2y));
 			file << ",b$" << std::hex << std::setw(2) << (int)(128 + (sin(rads1Real) * radius1x) + (cos(rads2Real) * radius2x));
 			file << std::endl;
 
@@ -51,37 +51,6 @@ int main(int argc, char**argv)
 		}
 		file << std::endl;
 
-		// TODO: While this works, it needs a proper copper
-#if 0
-		// Setup a wait for xpos ff ypos 160++ and transfer in new sprite data
-		double rads3Real = rads3;
-		int yoff = 0;
-		const int chunkSize = 24;
-		for (int chunk = 0; chunk < 24; chunk += chunkSize)
-		{
-			// Wait for raster and the position in the line
-			file << "d$0" << std::endl;
-			file << "w$ff01ff00,$" << std::hex << std::setw(2) << 150 + yoff;
-			file << "00c000" << std::endl;
-			yoff++;
-
-			// Output address for sprites
-			file << "s$" << std::hex << std::setw(4) << 0x9820 + (chunk*4);
-			file << "0100" << std::endl;
-
-			for (int i = 0; i < chunkSize; i++)
-			{
-				file << "b$" << std::hex << std::setw(2) << 0x10 + (i+chunk) + frame;
-				file << ",b$" << std::hex << std::setw(2) << (((i + chunk) / 8) & 0xf);
-				file << ",b$" << std::hex << std::setw(2) << 32 + (i + chunk);
-				file << ",b$" << std::hex << std::setw(2) << (int)(128 + (sin(rads3Real) * radius3x));
-				file << std::endl;
-
-				rads3Real += rads3Separation;
-			}
-			file << std::endl;
-		}
-#endif
 		// Output some copper bars that bounce
 		int colour = 1;
 		for (int i = 0; i < 64; i++)
@@ -100,6 +69,40 @@ int main(int argc, char**argv)
 		}
 		file << "d$9e030100" << std::endl;
 		file << "d$9e030100" << std::endl;
+
+
+		// TODO: While this works, it needs a proper copper
+#if 1
+		// Setup a wait for xpos ff ypos 160++ and transfer in new sprite data
+		double rads3Real = rads3;
+		int yoff = 0;
+		const int chunkSize = 24;
+		for (int chunk = 0; chunk < 24; chunk += chunkSize)
+		{
+			// Wait for raster and the position in the line
+			file << "d$0" << std::endl;
+			file << "w$ff01ff00,$" << std::hex << std::setw(2) << 160 + yoff;
+			file << "018000" << std::endl;
+			yoff++;
+
+			// Output address for sprites
+			file << "s$" << std::hex << std::setw(4) << 0x9820 + (chunk * 4);
+			file << "0100" << std::endl;
+
+			for (int i = 0; i < chunkSize; i++)
+			{
+				file << "b$" << std::hex << std::setw(2) << 0x10 + (i + chunk) + frame;
+				file << ",b$" << std::hex << std::setw(2) << (((i + chunk) / 8) & 0xf);
+				file << ",b$" << std::hex << std::setw(2) << 32 + (i + chunk);
+				file << ",b$" << std::hex << std::setw(2) << (int)(105 + (sin(rads3Real) * radius3x));
+				file << std::endl;
+
+				rads3Real += rads3Separation;
+			}
+			file << std::endl;
+		}
+#endif
+
 
 		// Output a screen scroll split
 		file << "d$0" << std::endl;
