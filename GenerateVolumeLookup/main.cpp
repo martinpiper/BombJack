ec2267789bf50aac1106218b3ea9883a8fc99e22
Martin Piper <martin.piper@gmail.com>
2020-06-10 23:20:59 +0800

    Adding left and right channel final mixing

--- GenerateVolumeLookup/main.cpp
+++ GenerateVolumeLookup/main.cpp
@@ -30,5 +30,35 @@ int main(int argc, char** argv)
 	}
 
 	fclose(fp);
+
+	fp = fopen("SampleCombine.bin" , "wb");
+
+	for (int b = 0 ; b < 256 ; b++)
+	{
+		for (int a = 0 ; a < 256 ; a++)
+		{
+			// Convert from u8 to s8 sample
+			int sampA = a - 0x80;
+			int sampB = b - 0x80;
+			int output = a + b;
+			// Convert from s8 to u8 sample
+			output = output + 0x80;
+
+			// And clamp
+			if (output > 255)
+			{
+				output = 255;
+			}
+			if (output < 0)
+			{
+				output = 0;
+			}
+
+			fputc(output , fp);
+		}
+	}
+
+	fclose(fp);
+
 	return 0;
 }
