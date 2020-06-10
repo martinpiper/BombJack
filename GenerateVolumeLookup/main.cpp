c07d033511f7ee151bbb05972f037eff9a7f4d71
Martin Piper <martin.piper@gmail.com>
2020-06-10 16:10:28 +0800

    Added extra step V0ST_OUTPUTADDR2 to allow V0ST_READRAM to be used for +ve edge trigger
    Volume calculation takes into account u8 format of samples

--- GenerateVolumeLookup/main.cpp
+++ GenerateVolumeLookup/main.cpp
@@ -5,15 +5,26 @@ int main(int argc, char** argv)
 {
 	FILE *fp = fopen("SampleVolume.bin" , "wb");
 
-	for (int a = 0 ; a < 256 ; a++)
+	for (int b = 0 ; b < 256 ; b++)
 	{
-		for (int b = 0 ; b < 256 ; b++)
+		for (int a = 0 ; a < 256 ; a++)
 		{
-			int output = (a * b) / 255;
+			// Convert from u8 to s8 sample
+			int samp = a - 0x80;
+			int output = (samp * b) / 255;
+			// Convert from s8 to u8 sample
+			output = output + 0x80;
+
+			// And clamp
 			if (output > 255)
 			{
 				output = 255;
 			}
+			if (output < 0)
+			{
+				output = 0;
+			}
+
 			fputc(output , fp);
 		}
 	}
