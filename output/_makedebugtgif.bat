f4f5b3594a9017f5e562c67e9ad6741e2844b7d4
Martin Piper <martin.piper@gmail.com>
2023-04-02 19:29:26 +0800

    APU debugging with new digital data generator

--- output/_makedebugtgif.bat
+++ output/_makedebugtgif.bat
@@ -1,1 +1,2 @@
-c:\Downloads\ImageMagick-7.0.7-4-portable-Q16-x64\convert.exe -loop 0 -delay 5 debug*.bmp debugt.gif && debugt.gif
+c:\Downloads\ImageMagick-7.0.7-4-portable-Q16-x64\convert.exe -loop 0 -delay 5 debug0*.bmp debugt.gif && debugt.gif
+c:\Downloads\ImageMagick-7.0.7-4-portable-Q16-x64\convert.exe -loop 0 -delay 5 debugold0*.bmp debugtold.gif && debugtold.gif
