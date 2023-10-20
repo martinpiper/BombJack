01f3e0a48f4ebfaf13c60a2298b2694476e82061
Martin Piper <martin.piper@gmail.com>
2023-10-20 19:29:46 +0800

    Update DigitalData part to match the new model build

--- AudioPlay.bat
+++ AudioPlay.bat
@@ -1,2 +1,2 @@
 c:\work\c64\bin\LZMPi.exe -hex C:\Work\BombJack\output\DebugAudioOutput.txt c:\temp\t.bin 1 2
-"C:\Users\Martin Piper\Downloads\ffmpeg-20200422-2e38c63-win64-static\ffmpeg-20200422-2e38c63-win64-static\bin\ffplay.exe" -f u8 -channels 2 -ar 25000 c:\temp\t.bin
+%USERPROFILE%\Downloads\ffmpeg-20200422-2e38c63-win64-static\ffmpeg-20200422-2e38c63-win64-static\bin\ffplay.exe -f u8 -channels 2 -ar 25000 c:\temp\t.bin
