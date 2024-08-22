3a6dcfbed56c86476a812b146bdc373f90022976
Martin Piper <martin.piper@gmail.com>
2024-08-22 18:43:10 +0800

    Update UserPort20To32Bit1_DataSend.py

--- UserPort20To32Bit1_DataSend.py
+++ UserPort20To32Bit1_DataSend.py
@@ -1,17 +1,32 @@
 import serial
+import time
 
 #serialPort = serial.Serial(port="COM5", baudrate=115200, parity=serial.PARITY_ODD, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 #serialPort = serial.Serial(port="COM5") # This seems to be just as fast as explicitly setting the speed
 serialPort = serial.Serial(port="COM5", baudrate=512000)
 
+# Using 1MHz
+# 256000  RAM bytes sent
+# 18195  bytes per second
+# Using 4MHz does not alter the transfer time, it is FTDI limited then, the DMA is four times faster though :)
+
+totalBytesSent = 0
+startTime = time.time()
+
 print(serialPort)
 
 
 def sendRawByte(value):
+    global totalBytesSent, startTime
     toSendWithEnable = bytearray([value | 0x40, value])
     # Setup data for the FTDI latches.
     # The no enable version is important, because it switches off the selector with the data still held, which causes a positive edge on the FTDI latches to load the held data.
     serialPort.write(toSendWithEnable)
+    totalBytesSent = totalBytesSent + 1
+    if ((totalBytesSent % 10240) == 0):
+        deltaTime = time.time() - startTime
+        if (deltaTime > 0):
+            print(int(totalBytesSent / deltaTime), " bytes per second")
 
 
 def sendFTDILatchNybble(latch, nybble):
@@ -65,8 +80,8 @@ while i < (256 * 1024):
     setDataByte(i / 16)       # Colour 1 white
     sendCompleteData()
     i = i + 1
-    if ((i % 1024) == 0):
-        print(i)
+    if ((i % 10240) == 0):
+        print(i, " RAM bytes sent")
         char = char + 1
 
 
