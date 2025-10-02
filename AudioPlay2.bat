7a9b2069452b0d1f1cb01c1043fc06069d20dc2f
Martin Piper <martin.piper@gmail.com>
2025-10-02 11:56:07 +0800

    Audio2: Increase rate range 62.5KHz is the maximum
    	Add power on reset
    	Add extra debug signals
    	Improve mixing output

---
+++ AudioPlay2.bat
@@ -0,0 +1,2 @@
+c:\work\c64\bin\LZMPi.exe -hex C:\Work\BombJack\output\DebugAudio2Output.txt c:\temp\t.bin 0 2
+%USERPROFILE%\Downloads\ffmpeg-20200422-2e38c63-win64-static\ffmpeg-20200422-2e38c63-win64-static\bin\ffplay.exe -f u8 -channels 2 -ar 25000 c:\temp\t.bin
