a7e0ae0a322897441b6aaa9987254a07ac1c02f6
Martin Piper <martin.piper@gmail.com>
2026-03-04 11:21:57 +0800

    Tested music output with proper max rate of 37313.432835821 Hz

--- AudioPlay3.bat
+++ AudioPlay3.bat
@@ -1,2 +1,2 @@
-c:\work\c64\bin\LZMPi.exe -hex C:\Work\BombJack\output\DebugAudio3Output.txt c:\temp\t.bin 0 2
-%USERPROFILE%\Downloads\ffmpeg-20200422-2e38c63-win64-static\ffmpeg-20200422-2e38c63-win64-static\bin\ffplay.exe -f u8 -channels 2 -ar 25000 c:\temp\t.bin
+c:\work\c64\bin\LZMPi.exe -hex C:\Work\BombJack\output\DebugAudio3Output.txt c:\temp\t.bin 2 4
+%USERPROFILE%\Downloads\ffmpeg-20200422-2e38c63-win64-static\ffmpeg-20200422-2e38c63-win64-static\bin\ffplay.exe -f u8 -channels 1 -ar 37313 c:\temp\t.bin
